ad936767913e1f693a2e71a4551afc5e55b88f37
Xojiakbar <87039534+XojiakbarA@users.noreply.github.com>
2023-09-08 20:40:46 +0500

    init

---
+++ src/test/java/uz/pdp/pcmarketdatarest/PcmarketDataRestApplicationTests.java
@@ -0,0 +1,13 @@
+package uz.pdp.pcmarketdatarest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PcmarketDataRestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
